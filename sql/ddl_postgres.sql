b21c940a674b71386deb9366942c5ce870b7360b
Mateus Nóbrega <mateus@nobrega.dev>
2022-07-26 00:01:05 -0300

    Formatação do arquivo

--- sql/ddl_postgres.sql
+++ sql/ddl_postgres.sql
@@ -1,360 +1,179 @@
--- public.arma definition
-
--- Drop table
-
--- DROP TABLE arma;
-
-CREATE TABLE arma (
-	idarma int4 NOT NULL,
-	dano int4 NULL,
-	maxbalas int4 NULL,
-	probacerto int4 NULL,
-	txdisparo int4 NULL,
-	nome bpchar(50) NULL,
-	tampente int4 NULL,
-	tipo bpchar(50) NULL,
-	penetracao int4 NULL,
-	tipomunicao bpchar(30) NULL,
-	CONSTRAINT arma_pk PRIMARY KEY (idarma)
+CREATE TABLE Arma (
+	idArma SERIAL PRIMARY KEY,
+	dano int4 NOT NULL,
+	maxBalas int4 NOT NULL,
+	probAcerto int4 NOT NULL,
+	txDisparo int4 NOT NULL,
+	nome varchar(50) NOT NULL,
+	tampente int4 NOT NULL,
+	tipo varchar(50) NOT NULL,
+	penetracao int4 NOT NULL,
+	tipoMunicao varchar(30) NOT NULL
 );
 
-
--- public.basemae definition
-
--- Drop table
-
--- DROP TABLE basemae;
-
-CREATE TABLE basemae (
-	idbasemae int4 NOT NULL,
-	produtomilitarbruto int4 NOT NULL,
-	idplayer int4 NOT NULL
+CREATE TABLE BaseMae (
+	idBaseMae SERIAL PRIMARY KEY,
+	produtoMilitarBruto int4 NOT NULL,
+	idPlayer int4 NOT NULL
 );
 
-
--- public.estatistica definition
-
--- Drop table
-
--- DROP TABLE estatistica;
-
-CREATE TABLE estatistica (
-	idestatistica int4 NOT NULL,
-	nummovimentos int4 NULL,
-	numheadshots int4 NULL,
-	numabates int4 NULL,
-	numnocautes int4 NULL,
-	codinome_nome varchar(30) NULL,
-	codinome_descricao varchar(300) NULL,
-	codinome_requisito varchar(200) NULL,
-	CONSTRAINT estatistica_pk PRIMARY KEY (idestatistica)
+CREATE TABLE Estatistica (
+	idEstatistica SERIAL PRIMARY KEY,
+	numMovimentos int4 NOT NULL,
+	numHeadshots int4 NOT NULL,
+	numAbates int4 NOT NULL,
+	numNocautes int4 NOT NULL,
+	codinomeNome varchar(30) NOT NULL,
+	codinomeDescricao varchar(300) NOT NULL,
+	codinomeRequisito varchar(200) NOT NULL
 );
 
-
--- public.item definition
-
--- Drop table
-
--- DROP TABLE item;
-
-CREATE TABLE item (
-	iditem int4 NOT NULL,
-	nome bpchar(30) NULL,
-	descricao varchar(100) NULL,
-	dano int4 NULL,
-	CONSTRAINT item_pk PRIMARY KEY (iditem)
+CREATE TABLE Item (
+	idItem SERIAL PRIMARY KEY,
+	nome varchar(30) NOT NULL,
+	descricao varchar(100) NOT NULL,
+	dano int4 NOT NULL
 );
 
-
--- public.mapa definition
-
--- Drop table
-
--- DROP TABLE mapa;
-
-CREATE TABLE mapa (
-	idmapa int4 NOT NULL,
-	tamanho int4 NULL,
-	nome bpchar(20) NULL,
-	CONSTRAINT mapa_pk PRIMARY KEY (idmapa)
+CREATE TABLE Mapa (
+	idMapa SERIAL PRIMARY KEY,
+	tamanho int4 NOT NULL,
+	nome varchar(20) NOT NULL
 );
 
-
--- public.mapaposicionarecurso definition
-
--- Drop table
-
--- DROP TABLE mapaposicionarecurso;
-
-CREATE TABLE mapaposicionarecurso (
-	idmapa int4 NULL,
-	idrecurso int4 NULL,
-	pontox int4 NULL,
-	pontoy int4 NULL,
-	quantidade int4 NULL
+CREATE TABLE MapaPosicionaRecurso (
+	idMapa SERIAL PRIMARY KEY,
+	idRecurso int4 NOT NULL,
+	pontoX int4 NOT NULL,
+	pontoY int4 NOT NULL,
+	quantidade int4 NOT NULL
 );
 
-
--- public.mapatemmapa definition
-
--- Drop table
-
--- DROP TABLE mapatemmapa;
-
-CREATE TABLE mapatemmapa (
-	idmapadono int4 NULL,
-	idmapa int4 NULL,
-	pontox int4 NULL,
-	pontoy int4 NULL
+CREATE TABLE MapaTemMapa (
+	idMapaDono int4 NOT NULL,
+	idMapa int4 NOT NULL,
+	pontoX int4 NOT NULL,
+	pontoY int4 NOT NULL
 );
 
-
--- public.missao definition
-
--- Drop table
-
--- DROP TABLE missao;
-
-CREATE TABLE missao (
-	idmissao int4 NOT NULL,
-	titulo bpchar(50) NULL,
-	descricao varchar(500) NULL,
-	textofinal text NULL,
-	CONSTRAINT missao_pk PRIMARY KEY (idmissao)
+CREATE TABLE Missao (
+	idMissao SERIAL PRIMARY KEY,
+	titulo varchar(50) NOT NULL,
+	descricao varchar(500) NOT NULL,
+	textoFinal text NOT NULL
 );
 
-
--- public.nivelnpc definition
-
--- Drop table
-
--- DROP TABLE nivelnpc;
-
-CREATE TABLE nivelnpc (
-	idnivel int4 NOT NULL,
-	desenvbase int4 NULL,
-	combate int4 NULL,
-	suporte int4 NULL,
-	medica int4 NULL,
-	inteligencia int4 NULL,
-	CONSTRAINT nivelnpc_pk PRIMARY KEY (idnivel)
+CREATE TABLE NivelNpc (
+	idNivel SERIAL PRIMARY KEY,
+	desenvBase int4 NOT NULL,
+	combate int4 NOT NULL,
+	suporte int4 NOT NULL,
+	medica int4 NOT NULL,
+	inteligencia int4 NOT NULL
 );
 
-
--- public.npc definition
-
--- Drop table
-
--- DROP TABLE npc;
-
-CREATE TABLE npc (
-	idnpc int4 NOT NULL,
-	nome bpchar(30) NULL,
-	categoria bpchar(30) NULL,
-	habilidade bpchar(30) NULL,
-	qtdvida int4 NULL,
-	idnivel int4 NULL,
-	iduniforme int4 NULL,
-	idunidade int4 NULL,
-	CONSTRAINT npc_pk PRIMARY KEY (idnpc)
+CREATE TABLE Npc (
+	idNpc SERIAL PRIMARY KEY,
+	nome varchar(30) NOT NULL,
+	categoria varchar(30) NOT NULL,
+	habilidade varchar(30) NOT NULL,
+	qtdVida int4 NOT NULL,
+	idNivel int4 NOT NULL,
+	idUniforme int4 NOT NULL,
+	idUnidade int4 NOT NULL
 );
 
-
--- public.npcequipaarma definition
-
--- Drop table
-
--- DROP TABLE npcequipaarma;
-
-CREATE TABLE npcequipaarma (
-	idnpc int4 NULL,
-	idarma int4 NULL
+CREATE TABLE NpcEquipaArma (
+	idNpc int4 NOT NULL,
+	idArma int4 NOT NULL
 );
 
-
--- public.npcusaitem definition
-
--- Drop table
-
--- DROP TABLE npcusaitem;
-
-CREATE TABLE npcusaitem (
-	idnpc int4 NULL,
-	iditem int4 NULL
+CREATE TABLE NpcUsaItem (
+	idNpc int4 NOT NULL,
+	idItem int4 NOT NULL
 );
 
-
--- public.objetivo definition
-
--- Drop table
-
--- DROP TABLE objetivo;
-
-CREATE TABLE objetivo (
-	idobjetivo int4 NOT NULL,
-	idmissao int4 NULL,
-	descricao varchar(200) NULL,
-	cumprido bool NULL,
-	tipo bool NULL,
-	pontox int4 NULL,
-	pontoy int4 NULL,
-	CONSTRAINT objetivo_pk PRIMARY KEY (idobjetivo)
+CREATE TABLE Objetivo (
+	idObjetivo SERIAL PRIMARY KEY,
+	idMissao int4 NOT NULL,
+	descricao varchar(200) NOT NULL,
+	cumprido bool NOT NULL,
+	tipo bool NOT NULL,
+	pontoX int4 NOT NULL,
+	pontoY int4 NOT NULL
 );
 
-
--- public.player definition
-
--- Drop table
-
--- DROP TABLE player;
-
-CREATE TABLE player (
-	idplayer int4 NOT NULL,
-	nome bpchar(30) NOT NULL,
-	idnpc int4 NULL,
-	CONSTRAINT player_pk PRIMARY KEY (idplayer)
+CREATE TABLE Player (
+	idPlayer SERIAL PRIMARY KEY,
+	nome varchar(30) NOT NULL,
+	idNpc int4 NOT NULL
 );
 
-
--- public.playercumpremissao definition
-
--- Drop table
-
--- DROP TABLE playercumpremissao;
-
-CREATE TABLE playercumpremissao (
-	idplayer int4 NOT NULL,
-	idestatistica int4 NULL,
-	idmissao int4 NULL
+CREATE TABLE PlayerCumpreMissao (
+	idPlayer int4 NOT NULL,
+	idEstatistica int4 NOT NULL,
+	idMissao int4 NOT NULL
 );
 
-
--- public.projeto definition
-
--- Drop table
-
--- DROP TABLE projeto;
-
-CREATE TABLE projeto (
-	idprojeto int4 NOT NULL,
-	idarma int4 NULL,
-	pontox int4 NULL,
-	pontoy int4 NULL,
-	idmapa int4 NULL,
-	iduniforme int4 NULL,
-	iditem int4 NULL,
-	CONSTRAINT projeto_pk PRIMARY KEY (idprojeto)
+CREATE TABLE Projeto (
+	idProjeto SERIAL PRIMARY KEY,
+	idArma int4 NOT NULL,
+	pontoX int4 NOT NULL,
+	pontoY int4 NOT NULL,
+	idMapa int4 NOT NULL,
+	idUniforme int4 NOT NULL,
+	idItem int4 NOT NULL
 );
 
-
--- public.recurso definition
-
--- Drop table
-
--- DROP TABLE recurso;
-
-CREATE TABLE recurso (
-	idrecurso int4 NOT NULL,
-	idbasemae int4 NULL,
-	qtdbruto int4 NULL,
-	qtdprocessado int4 NULL,
-	tipo bpchar(30) NULL,
-	CONSTRAINT recurso_pk PRIMARY KEY (idrecurso)
+CREATE TABLE Recurso (
+	idRecurso SERIAL PRIMARY KEY,
+	idBasemae int4 NOT NULL,
+	qtdBruto int4 NOT NULL,
+	qtdProcessado int4 NOT NULL,
+	tipo varchar(30) NOT NULL
 );
 
-
--- public.requisito definition
-
--- Drop table
-
--- DROP TABLE requisito;
-
-CREATE TABLE requisito (
-	idrequisito int4 NOT NULL,
-	tipo bool NULL,
-	idprojeto int4 NULL,
-	CONSTRAINT requisito_pk PRIMARY KEY (idrequisito)
+CREATE TABLE Requisito (
+	idRequisito SERIAL PRIMARY KEY,
+	tipo bool NOT NULL,
+	idProjeto int4 NOT NULL
 );
 
-
--- public.requisitoconsomerecurso definition
-
--- Drop table
-
--- DROP TABLE requisitoconsomerecurso;
-
-CREATE TABLE requisitoconsomerecurso (
-	idrecurso int4 NULL,
-	idrequisito int4 NULL,
-	quantidade int4 NULL
+CREATE TABLE RequisitoConsomeRecurso (
+	idRecurso int4 NOT NULL,
+	idRequisito int4 NOT NULL,
+	quantidade int4 NOT NULL
 );
 
-
--- public.requisitodependeunidade definition
-
--- Drop table
-
--- DROP TABLE requisitodependeunidade;
-
-CREATE TABLE requisitodependeunidade (
-	idunidade int4 NULL,
-	idrequisito varchar NULL,
-	nivelunidade int4 NULL
+CREATE TABLE RequisitoDependeUnidade (
+	idUnidade int4 NOT NULL,
+	idRequisito varchar NOT NULL,
+	nivelUnidade int4 NOT NULL
 );
 
-
--- public.terreno definition
-
--- Drop table
-
--- DROP TABLE terreno;
-
-CREATE TABLE terreno (
-	idterreno int4 NOT NULL,
-	descricao varchar(500) NULL,
-	vegetacao bpchar(50) NULL,
-	CONSTRAINT terreno_pk PRIMARY KEY (idterreno)
+CREATE TABLE Terreno (
+	idTerreno SERIAL PRIMARY KEY,
+	descricao varchar(500) NOT NULL,
+	vegetacao varchar(50) NOT NULL
 );
 
-
--- public.unidade definition
-
--- Drop table
-
--- DROP TABLE unidade;
-
-CREATE TABLE unidade (
-	idunidade int4 NULL,
-	nivel int4 NULL,
-	qtdsoldados int4 NULL,
-	tipo bpchar(30) NULL,
-	maxsoldados int4 NULL,
-	idbasemae int4 NULL
+CREATE TABLE Unidade (
+	idUnidade SERIAL PRIMARY KEY,
+	nivel int4 NOT NULL,
+	qtdSoldados int4 NOT NULL,
+	tipo varchar(30) NOT NULL,
+	maxSoldados int4 NOT NULL,
+	idBaseMae int4 NOT NULL
 );
 
-
--- public.uniforme definition
-
--- Drop table
-
--- DROP TABLE uniforme;
-
-CREATE TABLE uniforme (
-	iduniforme int4 NOT NULL,
-	nome bpchar(30) NULL,
-	defesa int4 NULL,
-	CONSTRAINT uniforme_pk PRIMARY KEY (iduniforme)
+CREATE TABLE Uniforme (
+	idUniforme SERIAL PRIMARY KEY,
+	nome varchar(30) NOT NULL,
+	defesa int4 NOT NULL
 );
 
-
--- public.uniformecamuflaterreno definition
-
--- Drop table
-
--- DROP TABLE uniformecamuflaterreno;
-
-CREATE TABLE uniformecamuflaterreno (
-	idterreno int4 NULL,
-	iduniforme int4 NULL,
-	taxacamuflagem int4 NULL
+CREATE TABLE UniformeCamuflaTerreno (
+	idTerreno int4 NOT NULL,
+	idUniforme int4 NOT NULL,
+	taxaCamuflagem int4 NOT NULL
 );
