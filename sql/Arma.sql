8b2bf6f1abb52aaf0d4ed1da6d5189c40a1c6bf2
Lucas Queiroz <86672557+lucasqueiroz23@users.noreply.github.com>
2022-07-27 11:51:39 -0300

    Criação do arquivo Arma.sql e adição de inserts.
    Co-authored-by:João Henrique<joao.henrique1299@hotmail.com>

---
+++ sql/Arma.sql
@@ -0,0 +1,9 @@
+INSERT INTO Arma (
+nome,dano,maxBalas,tamPente,probAcerto,txDisparo,tipo,penetracao,municaoLetal)
+VALUES
+('AK-47',33,120,30,20,5,'PRIMARIA',TRUE),
+('M16',20,120,30,25,3,'PRIMARIA',TRUE),
+('GLOCK',15,80,20,15,3,'SECUNDARIA',TRUE),
+('.38',100,30,6,50,1,'SECUNDARIA',TRUE),
+('TASER',100,5,15,50,1,'SECUNDARIA',FALSE);
+
