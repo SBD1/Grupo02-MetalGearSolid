89d4c76b6b361e0134a19035403348cb89877217
Mateus Nóbrega <mateus@nobrega.dev>
2022-07-26 23:57:20 -0300

    Adição de arquivo nao formatado com FKs, falta fazer modificações especificadas nas issues

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,55 +1,179 @@
---- URI Online Judge SQL
---- Copyright URI Online Judge
---- www.urionlinejudge.com.br
---- Problem 2619
-
-CREATE TABLE providers (
-  id numeric PRIMARY KEY,
-  name varchar(255),
-  street varchar(255),
-  city varchar(255),
-  state char(2)
+CREATE TABLE Arma (
+	idArma SERIAL PRIMARY KEY,
+	dano int4 NOT NULL,
+	maxBalas int4 NOT NULL,
+	probAcerto int4 NOT NULL,
+	txDisparo int4 NOT NULL,
+	nome varchar(50) NOT NULL,
+	tampente int4 NOT NULL,
+	tipo varchar(50) NOT NULL,
+	penetracao int4 NOT NULL,
+	tipoMunicao varchar(30) NOT NULL
 );
 
-CREATE TABLE categories (
-  id numeric PRIMARY KEY,
-  name varchar(255)
+CREATE TABLE BaseMae (
+	idBaseMae SERIAL PRIMARY KEY,
+	produtoMilitarBruto int4 NOT NULL,
+	idPlayer int4 NOT NULL
 );
 
-CREATE TABLE products (
-  id numeric PRIMARY KEY,
-  name varchar (255),
-  amount numeric,
-  price numeric,
-  id_providers numeric REFERENCES providers (id),
-  id_categories numeric REFERENCES categories (id)
+CREATE TABLE Estatistica (
+	idEstatistica SERIAL PRIMARY KEY,
+	numMovimentos int4 NOT NULL,
+	numHeadshots int4 NOT NULL,
+	numAbates int4 NOT NULL,
+	numNocautes int4 NOT NULL,
+	codinomeNome varchar(30) NOT NULL,
+	codinomeDescricao varchar(300) NOT NULL,
+	codinomeRequisito varchar(200) NOT NULL
 );
 
+CREATE TABLE Item (
+	idItem SERIAL PRIMARY KEY,
+	nome varchar(30) NOT NULL,
+	descricao varchar(100) NOT NULL,
+	dano int4 NOT NULL
+);
 
-INSERT INTO providers (id, name, street, city, state)
-VALUES
-  (1,	'Ajax SA',	'Rua Presidente Castelo Branco',	'Porto Alegre',	'RS'),
-  (2,	'Sansul SA',	'Av Brasil',	'Rio de Janeiro',	'RJ'),
-  (3,	'South Chairs',	'Rua do Moinho',	'Santa Maria',	'RS'),
-  (4,	'Elon Electro',	'Rua Apolo',	'São Paulo',	'SP'),
-  (5,	'Mike electro',	'Rua Pedro da Cunha',	'Curitiba',	'PR');
-  
-INSERT INTO categories (id, name)
-VALUES
-  (1,	'Super Luxury'),
-  (2,	'Imported'),
-  (3,	'Tech'),
-  (4,	'Vintage'),
-  (5,	'Supreme');
-  
-INSERT INTO products ( id, name, amount, price, id_providers, id_categories)
-VALUES
-  (1,	'Blue Chair',	30,	300.00,	5,	5),
-  (2,	'Red Chair',	50,	2150.00,	2,	1),
-  (3,	'Disney Wardrobe',	400,	829.50,	4,	1),
-  (4,	'Blue Toaster',	20,	9.90,	3,	1),
-  (5,	'TV',	30,	3000.25,	2,	2);
-  
-  
-/*  Execute this query to drop the tables */
--- DROP TABLE products, categories, providers; --
+CREATE TABLE Mapa (
+	idMapa SERIAL PRIMARY KEY,
+	tamanho int4 NOT NULL,
+	nome varchar(20) NOT NULL
+);
+
+CREATE TABLE MapaPosicionaRecurso (
+	idMapa SERIAL PRIMARY KEY,
+	idRecurso int4 NOT NULL,
+	pontoX int4 NOT NULL,
+	pontoY int4 NOT NULL,
+	quantidade int4 NOT NULL
+);
+
+CREATE TABLE MapaTemMapa (
+	idMapaDono int4 NOT NULL,
+	idMapa int4 NOT NULL,
+	pontoX int4 NOT NULL,
+	pontoY int4 NOT NULL
+);
+
+CREATE TABLE Missao (
+	idMissao SERIAL PRIMARY KEY,
+	titulo varchar(50) NOT NULL,
+	descricao varchar(500) NOT NULL,
+	textoFinal text NOT NULL
+);
+
+CREATE TABLE NivelNpc (
+	idNivel SERIAL PRIMARY KEY,
+	desenvBase int4 NOT NULL,
+	combate int4 NOT NULL,
+	suporte int4 NOT NULL,
+	medica int4 NOT NULL,
+	inteligencia int4 NOT NULL
+);
+
+CREATE TABLE Npc (
+	idNpc SERIAL PRIMARY KEY,
+	nome varchar(30) NOT NULL,
+	categoria varchar(30) NOT NULL,
+	habilidade varchar(30) NOT NULL,
+	qtdVida int4 NOT NULL,
+	idNivel int4 NOT NULL,
+	idUniforme int4 NOT NULL,
+	idUnidade int4 NOT NULL
+);
+
+CREATE TABLE NpcEquipaArma (
+	idNpc int4 NOT NULL,
+	idArma int4 NOT NULL
+);
+
+CREATE TABLE NpcUsaItem (
+	idNpc int4 NOT NULL,
+	idItem int4 NOT NULL
+);
+
+CREATE TABLE Objetivo (
+	idObjetivo SERIAL PRIMARY KEY,
+	idMissao int4 NOT NULL,
+	descricao varchar(200) NOT NULL,
+	cumprido bool NOT NULL,
+	tipo bool NOT NULL,
+	pontoX int4 NOT NULL,
+	pontoY int4 NOT NULL
+);
+
+CREATE TABLE Player (
+	idPlayer SERIAL PRIMARY KEY,
+	nome varchar(30) NOT NULL,
+	idNpc int4 NOT NULL
+);
+
+CREATE TABLE PlayerCumpreMissao (
+	idPlayer int4 NOT NULL,
+	idEstatistica int4 NOT NULL,
+	idMissao int4 NOT NULL
+);
+
+CREATE TABLE Projeto (
+	idProjeto SERIAL PRIMARY KEY,
+	idArma int4 NOT NULL,
+	pontoX int4 NOT NULL,
+	pontoY int4 NOT NULL,
+	idMapa int4 NOT NULL,
+	idUniforme int4 NOT NULL,
+	idItem int4 NOT NULL
+);
+
+CREATE TABLE Recurso (
+	idRecurso SERIAL PRIMARY KEY,
+	idBasemae int4 NOT NULL,
+	qtdBruto int4 NOT NULL,
+	qtdProcessado int4 NOT NULL,
+	tipo varchar(30) NOT NULL
+);
+
+CREATE TABLE Requisito (
+	idRequisito SERIAL PRIMARY KEY,
+	tipo bool NOT NULL,
+	idProjeto int4 NOT NULL
+);
+
+CREATE TABLE RequisitoConsomeRecurso (
+	idRecurso int4 NOT NULL,
+	idRequisito int4 NOT NULL,
+	quantidade int4 NOT NULL
+);
+
+CREATE TABLE RequisitoDependeUnidade (
+	idUnidade int4 NOT NULL,
+	idRequisito varchar NOT NULL,
+	nivelUnidade int4 NOT NULL
+);
+
+CREATE TABLE Terreno (
+	idTerreno SERIAL PRIMARY KEY,
+	descricao varchar(500) NOT NULL,
+	vegetacao varchar(50) NOT NULL
+);
+
+CREATE TABLE Unidade (
+	idUnidade SERIAL PRIMARY KEY,
+	nivel int4 NOT NULL,
+	qtdSoldados int4 NOT NULL,
+	tipo varchar(30) NOT NULL,
+	maxSoldados int4 NOT NULL,
+	idBaseMae int4 NOT NULL
+);
+
+CREATE TABLE Uniforme (
+	idUniforme SERIAL PRIMARY KEY,
+	nome varchar(30) NOT NULL,
+	defesa int4 NOT NULL
+);
+
+CREATE TABLE UniformeCamuflaTerreno (
+	idTerreno int4 NOT NULL,
+	idUniforme int4 NOT NULL,
+	taxaCamuflagem int4 NOT NULL
+);
--- sql/ddl_postgres.sql
+++ sql/ddl_postgres.sql
@@ -1,179 +1,224 @@
-CREATE TABLE Arma (
-	idArma SERIAL PRIMARY KEY,
+CREATE TABLE arma (
+	idarma serial4 NOT NULL,
 	dano int4 NOT NULL,
-	maxBalas int4 NOT NULL,
-	probAcerto int4 NOT NULL,
-	txDisparo int4 NOT NULL,
+	maxbalas int4 NOT NULL,
+	probacerto int4 NOT NULL,
+	txdisparo int4 NOT NULL,
 	nome varchar(50) NOT NULL,
 	tampente int4 NOT NULL,
 	tipo varchar(50) NOT NULL,
 	penetracao int4 NOT NULL,
-	tipoMunicao varchar(30) NOT NULL
+	tipomunicao varchar(30) NOT NULL,
+	CONSTRAINT arma_pkey PRIMARY KEY (idarma)
 );
 
-CREATE TABLE BaseMae (
-	idBaseMae SERIAL PRIMARY KEY,
-	produtoMilitarBruto int4 NOT NULL,
-	idPlayer int4 NOT NULL
+CREATE TABLE estatistica (
+	idestatistica serial4 NOT NULL,
+	nummovimentos int4 NOT NULL,
+	numheadshots int4 NOT NULL,
+	numabates int4 NOT NULL,
+	numnocautes int4 NOT NULL,
+	codinomenome varchar(30) NOT NULL,
+	codinomedescricao varchar(300) NOT NULL,
+	codinomerequisito varchar(200) NOT NULL,
+	CONSTRAINT estatistica_pkey PRIMARY KEY (idestatistica)
 );
 
-CREATE TABLE Estatistica (
-	idEstatistica SERIAL PRIMARY KEY,
-	numMovimentos int4 NOT NULL,
-	numHeadshots int4 NOT NULL,
-	numAbates int4 NOT NULL,
-	numNocautes int4 NOT NULL,
-	codinomeNome varchar(30) NOT NULL,
-	codinomeDescricao varchar(300) NOT NULL,
-	codinomeRequisito varchar(200) NOT NULL
-);
-
-CREATE TABLE Item (
-	idItem SERIAL PRIMARY KEY,
+CREATE TABLE item (
+	iditem serial4 NOT NULL,
 	nome varchar(30) NOT NULL,
 	descricao varchar(100) NOT NULL,
-	dano int4 NOT NULL
+	dano int4 NOT NULL,
+	CONSTRAINT item_pkey PRIMARY KEY (iditem)
 );
 
-CREATE TABLE Mapa (
-	idMapa SERIAL PRIMARY KEY,
+CREATE TABLE mapa (
+	idmapa serial4 NOT NULL,
 	tamanho int4 NOT NULL,
-	nome varchar(20) NOT NULL
+	nome varchar(20) NOT NULL,
+	CONSTRAINT mapa_pkey PRIMARY KEY (idmapa)
 );
 
-CREATE TABLE MapaPosicionaRecurso (
-	idMapa SERIAL PRIMARY KEY,
-	idRecurso int4 NOT NULL,
-	pontoX int4 NOT NULL,
-	pontoY int4 NOT NULL,
-	quantidade int4 NOT NULL
-);
-
-CREATE TABLE MapaTemMapa (
-	idMapaDono int4 NOT NULL,
-	idMapa int4 NOT NULL,
-	pontoX int4 NOT NULL,
-	pontoY int4 NOT NULL
-);
-
-CREATE TABLE Missao (
-	idMissao SERIAL PRIMARY KEY,
+CREATE TABLE missao (
+	idmissao serial4 NOT NULL,
 	titulo varchar(50) NOT NULL,
 	descricao varchar(500) NOT NULL,
-	textoFinal text NOT NULL
+	textofinal text NOT NULL,
+	CONSTRAINT missao_pkey PRIMARY KEY (idmissao)
 );
 
-CREATE TABLE NivelNpc (
-	idNivel SERIAL PRIMARY KEY,
-	desenvBase int4 NOT NULL,
+CREATE TABLE nivelnpc (
+	idnivel serial4 NOT NULL,
+	desenvbase int4 NOT NULL,
 	combate int4 NOT NULL,
 	suporte int4 NOT NULL,
 	medica int4 NOT NULL,
-	inteligencia int4 NOT NULL
+	inteligencia int4 NOT NULL,
+	CONSTRAINT nivelnpc_pkey PRIMARY KEY (idnivel)
 );
 
-CREATE TABLE Npc (
-	idNpc SERIAL PRIMARY KEY,
+CREATE TABLE terreno (
+	idterreno serial4 NOT NULL,
+	descricao varchar(500) NOT NULL,
+	vegetacao varchar(50) NOT NULL,
+	CONSTRAINT terreno_pkey PRIMARY KEY (idterreno)
+);
+
+CREATE TABLE uniforme (
+	iduniforme serial4 NOT NULL,
 	nome varchar(30) NOT NULL,
-	categoria varchar(30) NOT NULL,
-	habilidade varchar(30) NOT NULL,
-	qtdVida int4 NOT NULL,
-	idNivel int4 NOT NULL,
-	idUniforme int4 NOT NULL,
-	idUnidade int4 NOT NULL
+	defesa int4 NOT NULL,
+	CONSTRAINT uniforme_pkey PRIMARY KEY (iduniforme)
 );
 
-CREATE TABLE NpcEquipaArma (
-	idNpc int4 NOT NULL,
-	idArma int4 NOT NULL
+CREATE TABLE mapatemmapa (
+	idmapadono int4 NOT NULL,
+	idmapa int4 NOT NULL,
+	pontox int4 NOT NULL,
+	pontoy int4 NOT NULL,
+	CONSTRAINT mapatemmapa_fk FOREIGN KEY (idmapa) REFERENCES mapa(idmapa) ON DELETE RESTRICT,
+	CONSTRAINT mapatemmapa_fk_1 FOREIGN KEY (idmapadono) REFERENCES mapa(idmapa) ON DELETE RESTRICT
 );
 
-CREATE TABLE NpcUsaItem (
-	idNpc int4 NOT NULL,
-	idItem int4 NOT NULL
-);
-
-CREATE TABLE Objetivo (
-	idObjetivo SERIAL PRIMARY KEY,
-	idMissao int4 NOT NULL,
+CREATE TABLE objetivo (
+	idobjetivo serial4 NOT NULL,
+	idmissao int4 NOT NULL,
 	descricao varchar(200) NOT NULL,
 	cumprido bool NOT NULL,
 	tipo bool NOT NULL,
-	pontoX int4 NOT NULL,
-	pontoY int4 NOT NULL
+	pontox int4 NOT NULL,
+	pontoy int4 NOT NULL,
+	CONSTRAINT objetivo_pkey PRIMARY KEY (idobjetivo),
+	CONSTRAINT objetivo_fk FOREIGN KEY (idmissao) REFERENCES missao(idmissao) ON DELETE RESTRICT
 );
 
-CREATE TABLE Player (
-	idPlayer SERIAL PRIMARY KEY,
-	nome varchar(30) NOT NULL,
-	idNpc int4 NOT NULL
+CREATE TABLE projeto (
+	idprojeto serial4 NOT NULL,
+	idarma int4 NOT NULL,
+	pontox int4 NOT NULL,
+	pontoy int4 NOT NULL,
+	idmapa int4 NOT NULL,
+	iduniforme int4 NOT NULL,
+	iditem int4 NOT NULL,
+	CONSTRAINT projeto_pkey PRIMARY KEY (idprojeto),
+	CONSTRAINT projeto_fk FOREIGN KEY (idarma) REFERENCES arma(idarma) ON DELETE RESTRICT,
+	CONSTRAINT projeto_fk_1 FOREIGN KEY (idmapa) REFERENCES mapa(idmapa) ON DELETE RESTRICT,
+	CONSTRAINT projeto_fk_2 FOREIGN KEY (iduniforme) REFERENCES uniforme(iduniforme) ON DELETE RESTRICT,
+	CONSTRAINT projeto_fk_3 FOREIGN KEY (iditem) REFERENCES item(iditem) ON DELETE RESTRICT
 );
 
-CREATE TABLE PlayerCumpreMissao (
-	idPlayer int4 NOT NULL,
-	idEstatistica int4 NOT NULL,
-	idMissao int4 NOT NULL
-);
-
-CREATE TABLE Projeto (
-	idProjeto SERIAL PRIMARY KEY,
-	idArma int4 NOT NULL,
-	pontoX int4 NOT NULL,
-	pontoY int4 NOT NULL,
-	idMapa int4 NOT NULL,
-	idUniforme int4 NOT NULL,
-	idItem int4 NOT NULL
-);
-
-CREATE TABLE Recurso (
-	idRecurso SERIAL PRIMARY KEY,
-	idBasemae int4 NOT NULL,
-	qtdBruto int4 NOT NULL,
-	qtdProcessado int4 NOT NULL,
-	tipo varchar(30) NOT NULL
-);
-
-CREATE TABLE Requisito (
-	idRequisito SERIAL PRIMARY KEY,
+CREATE TABLE requisito (
+	idrequisito serial4 NOT NULL,
 	tipo bool NOT NULL,
-	idProjeto int4 NOT NULL
+	idprojeto int4 NOT NULL,
+	CONSTRAINT requisito_pkey PRIMARY KEY (idrequisito),
+	CONSTRAINT requisito_fk FOREIGN KEY (idprojeto) REFERENCES projeto(idprojeto) ON DELETE RESTRICT
 );
 
-CREATE TABLE RequisitoConsomeRecurso (
-	idRecurso int4 NOT NULL,
-	idRequisito int4 NOT NULL,
-	quantidade int4 NOT NULL
+CREATE TABLE uniformecamuflaterreno (
+	idterreno int4 NOT NULL,
+	iduniforme int4 NOT NULL,
+	taxacamuflagem int4 NOT NULL,
+	CONSTRAINT uniformecamuflaterreno_fk FOREIGN KEY (idterreno) REFERENCES terreno(idterreno) ON DELETE RESTRICT,
+	CONSTRAINT uniformecamuflaterreno_fk_1 FOREIGN KEY (iduniforme) REFERENCES uniforme(iduniforme) ON DELETE RESTRICT
 );
 
-CREATE TABLE RequisitoDependeUnidade (
-	idUnidade int4 NOT NULL,
-	idRequisito varchar NOT NULL,
-	nivelUnidade int4 NOT NULL
+CREATE TABLE basemae (
+	idbasemae serial4 NOT NULL,
+	produtomilitarbruto int4 NOT NULL,
+	idplayer int4 NOT NULL,
+	CONSTRAINT basemae_pkey PRIMARY KEY (idbasemae),
+	CONSTRAINT basemae_fk FOREIGN KEY (idplayer) REFERENCES player(idplayer) ON DELETE RESTRICT
 );
 
-CREATE TABLE Terreno (
-	idTerreno SERIAL PRIMARY KEY,
-	descricao varchar(500) NOT NULL,
-	vegetacao varchar(50) NOT NULL
+CREATE TABLE mapaposicionarecurso (
+	idmapa int4 NOT NULL,
+	idrecurso int4 NOT NULL,
+	pontox int4 NOT NULL,
+	pontoy int4 NOT NULL,
+	quantidade int4 NOT NULL,
+	CONSTRAINT mapaposicionarecurso_fk FOREIGN KEY (idmapa) REFERENCES mapa(idmapa) ON DELETE RESTRICT,
+	CONSTRAINT mapaposicionarecurso_fk_1 FOREIGN KEY (idrecurso) REFERENCES recurso(idrecurso) ON DELETE RESTRICT
 );
 
-CREATE TABLE Unidade (
-	idUnidade SERIAL PRIMARY KEY,
-	nivel int4 NOT NULL,
-	qtdSoldados int4 NOT NULL,
-	tipo varchar(30) NOT NULL,
-	maxSoldados int4 NOT NULL,
-	idBaseMae int4 NOT NULL
-);
-
-CREATE TABLE Uniforme (
-	idUniforme SERIAL PRIMARY KEY,
+CREATE TABLE npc (
+	idnpc serial4 NOT NULL,
 	nome varchar(30) NOT NULL,
-	defesa int4 NOT NULL
+	categoria varchar(30) NOT NULL,
+	habilidade varchar(30) NOT NULL,
+	qtdvida int4 NOT NULL,
+	idnivel int4 NOT NULL,
+	iduniforme int4 NOT NULL,
+	idunidade int4 NOT NULL,
+	CONSTRAINT npc_pkey PRIMARY KEY (idnpc),
+	CONSTRAINT npc_fk FOREIGN KEY (idnivel) REFERENCES nivelnpc(idnivel) ON DELETE RESTRICT,
+	CONSTRAINT npc_fk_1 FOREIGN KEY (iduniforme) REFERENCES uniforme(iduniforme) ON DELETE RESTRICT,
+	CONSTRAINT npc_fk_2 FOREIGN KEY (idunidade) REFERENCES unidade(idunidade) ON DELETE RESTRICT
 );
 
-CREATE TABLE UniformeCamuflaTerreno (
-	idTerreno int4 NOT NULL,
-	idUniforme int4 NOT NULL,
-	taxaCamuflagem int4 NOT NULL
+CREATE TABLE npcequipaarma (
+	idnpc int4 NOT NULL,
+	idarma int4 NOT NULL,
+	CONSTRAINT npcequipaarma_fk FOREIGN KEY (idnpc) REFERENCES npc(idnpc) ON DELETE RESTRICT,
+	CONSTRAINT npcequipaarma_fk_1 FOREIGN KEY (idarma) REFERENCES arma(idarma) ON DELETE RESTRICT
+);
+
+CREATE TABLE npcusaitem (
+	idnpc int4 NOT NULL,
+	iditem int4 NOT NULL,
+	CONSTRAINT npcusaitem_fk FOREIGN KEY (idnpc) REFERENCES npc(idnpc) ON DELETE RESTRICT,
+	CONSTRAINT npcusaitem_fk_1 FOREIGN KEY (iditem) REFERENCES item(iditem) ON DELETE RESTRICT
+);
+
+CREATE TABLE player (
+	idplayer serial4 NOT NULL,
+	nome varchar(30) NOT NULL,
+	idnpc int4 NOT NULL,
+	CONSTRAINT player_pkey PRIMARY KEY (idplayer),
+	CONSTRAINT player_fk FOREIGN KEY (idnpc) REFERENCES npc(idnpc) ON DELETE RESTRICT
+);
+
+CREATE TABLE playercumpremissao (
+	idplayer int4 NOT NULL,
+	idestatistica int4 NOT NULL,
+	idmissao int4 NOT NULL,
+	CONSTRAINT playercumpremissao_fk FOREIGN KEY (idplayer) REFERENCES player(idplayer) ON DELETE RESTRICT,
+	CONSTRAINT playercumpremissao_fk_1 FOREIGN KEY (idestatistica) REFERENCES estatistica(idestatistica) ON DELETE RESTRICT,
+	CONSTRAINT playercumpremissao_fk_2 FOREIGN KEY (idmissao) REFERENCES missao(idmissao) ON DELETE RESTRICT
+);
+
+CREATE TABLE recurso (
+	idrecurso serial4 NOT NULL,
+	idbasemae int4 NOT NULL,
+	qtdbruto int4 NOT NULL,
+	qtdprocessado int4 NOT NULL,
+	tipo varchar(30) NOT NULL,
+	CONSTRAINT recurso_pkey PRIMARY KEY (idrecurso),
+	CONSTRAINT recurso_fk FOREIGN KEY (idbasemae) REFERENCES basemae(idbasemae) ON DELETE RESTRICT
+);
+
+CREATE TABLE requisitoconsomerecurso (
+	idrecurso int4 NOT NULL,
+	idrequisito int4 NOT NULL,
+	quantidade int4 NOT NULL,
+	CONSTRAINT requisitoconsomerecurso_fk FOREIGN KEY (idrecurso) REFERENCES recurso(idrecurso) ON DELETE RESTRICT,
+	CONSTRAINT requisitoconsomerecurso_fk_1 FOREIGN KEY (idrequisito) REFERENCES requisito(idrequisito) ON DELETE RESTRICT
+);
+
+CREATE TABLE requisitodependeunidade (
+	idunidade int4 NOT NULL,
+	idrequisito varchar NOT NULL,
+	nivelunidade int4 NOT NULL,
+	CONSTRAINT requisitodependeunidade_fk FOREIGN KEY (idunidade) REFERENCES unidade(idunidade) ON DELETE RESTRICT
+);
+
+CREATE TABLE unidade (
+	idunidade serial4 NOT NULL,
+	nivel int4 NOT NULL,
+	qtdsoldados int4 NOT NULL,
+	tipo varchar(30) NOT NULL,
+	maxsoldados int4 NOT NULL,
+	idbasemae int4 NOT NULL,
+	CONSTRAINT unidade_pkey PRIMARY KEY (idunidade),
+	CONSTRAINT unidade_fk FOREIGN KEY (idbasemae) REFERENCES basemae(idbasemae) ON DELETE RESTRICT
 );
