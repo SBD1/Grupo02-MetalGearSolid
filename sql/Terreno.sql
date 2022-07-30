6c680f41301b9ec602adcc76c528679007ee0458
RafaelBP02 <rafaelbertopereira@gmail.com>
2022-07-30 16:34:33 -0300

    Completando uma parte da Issue #10: Inserindo terrenos

---
+++ sql/Terreno.sql
@@ -0,0 +1,8 @@
+insert into Terreno
+(idTerreno, descricao, vegetacao)
+values
+(1, Representa uma cidade ou um interior de uma edificacao, Urbano)
+(2, Representa um ambiente em que possui uma fabrica ou industria. Tabem representa seus respecrivos interiores, Industrial)
+(3, Representa ambientes arenosos ou gelidos. Ambientes abertos e com pouquissimas plantas, Deserto)
+(4, Representa uma parte do continente africano, Africa Central)
+(5, Representa ambientes com uma abundancia gigantesta de platas e alta umidade, Floresta);
--- sql/dml.sql
+++ sql/dml.sql
@@ -4,3 +4,4 @@
 \i docker-entrypoint-initdb.d/Unidade.sql
 \i docker-entrypoint-initdb.d/codinome.sql
 \i docker-entrypoint-initdb.d/NPC.sql
+\i docker-entrypoint-initdb.d/Terreno.sql
