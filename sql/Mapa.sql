a7fcf1d71d5f74f1461f4563e0ff4dbeb9737d17
Mateus Nóbrega <mateus@nobrega.dev>
2022-08-01 20:05:25 -0300

    (#11) Arrumando inserts e adicionando Objetivos

--- sql/Mapa.sql
+++ sql/Mapa.sql
@@ -1,7 +1,7 @@
-INSERT INTO Mapa (idMapa, tamanho, nome)
+INSERT INTO Mapa (idMapa, tamanho, nome, idMissao)
 VALUES
-(1, 30, 'Amaznôia'),
-(2, 2, 'Casa na Árvore Abandonada'),
-(3, 30, 'Kabul, Afeganistao'),
-(4, 10, 'Base Inimiga'),
-(5, 30, 'Serengeti, Tanzania');
+(1, 30, 'Amazônia', 1),
+(2, 2, 'Casa na Árvore Abandonada', 2),
+(3, 30, 'Kabul, Afeganistao', 3),
+(4, 10, 'Base Inimiga', 4),
+(5, 30, 'Serengeti, Tanzania', 5);
