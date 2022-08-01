6a62287ce0b041a296d4a06ea95967616ec5579c
RafaelBP02 <rafaelbertopereira@gmail.com>
2022-08-01 10:33:38 -0300

    Bug fix - Issue #22 : Corrigindo sintaxe das insercoes sql e erros de portugues

--- sql/Mapa.sql
+++ sql/Mapa.sql
@@ -1,7 +1,7 @@
 INSERT INTO Mapa (idMapa, tamanho, nome)
 VALUES
-(1, 30, Amazonia),
-(2, 2, Casa na Arvore Abandonada),
-(3, 30, Kabul_Afeganistao),
-(4, 10, Base Inimiga),
-(5, 30, Serengeti_Tanzania);
+(1, 30, 'Amaznôia'),
+(2, 2, 'Casa na Árvore Abandonada'),
+(3, 30, 'Kabul, Afeganistao'),
+(4, 10, 'Base Inimiga'),
+(5, 30, 'Serengeti, Tanzania');
--- sql/Terreno.sql
+++ sql/Terreno.sql
@@ -1,8 +1,8 @@
-insert into Terreno
+INSERT INTO Terreno
 (idTerreno, descricao, vegetacao)
-values
-(1, Representa uma cidade ou um interior de uma edificacao, Urbano)
-(2, Representa um ambiente em que possui uma fabrica ou industria. Tabem representa seus respecrivos interiores, Industrial)
-(3, Representa ambientes arenosos ou gelidos. Ambientes abertos e com pouquissimas plantas, Deserto)
-(4, Representa uma parte do continente africano, Africa Central)
-(5, Representa ambientes com uma abundancia gigantesta de platas e alta umidade, Floresta);
+VALUES
+(1, 'Representa uma cidade ou um interior de uma edificacao', 'Urbano'),
+(2, 'Representa um ambiente que possui uma fabrica ou indústria e seus respecrivos interiores', 'Industrial'),
+(3, 'Representa ambientes arenosos ou gelidos, ambientes abertos e com pouquíssimas plantas', 'Deserto'),
+(4, 'Representa uma parte do continente africano', 'Africa Central'),
+(5, 'Representa ambientes com uma abundancia gigantesta de platas e alta umidade', 'Floresta');
