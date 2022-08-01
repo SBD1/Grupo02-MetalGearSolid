74f395085d686d3f552a6563edf8655329772545
Lucas Queiroz <86672557+lucasqueiroz23@users.noreply.github.com>
2022-08-01 14:07:58 -0300

    terminada #16

---
+++ dml.sql
@@ -0,0 +1,13 @@
+\i docker-entrypoint-initdb.d/Arma.sql
+\i docker-entrypoint-initdb.d/BaseMae.sql
+\i docker-entrypoint-initdb.d/Recurso.sql
+\i docker-entrypoint-initdb.d/Unidade.sql
+\i docker-entrypoint-initdb.d/codinome.sql
+\i docker-entrypoint-initdb.d/Terreno.sql
+\i docker-entrypoint-initdb.d/Mapa.sql
+\i docker-entrypoint-initdb.d/Uniforme.sql
+\i docker-entrypoint-initdb.d/UniformeCamuflaTerreno.sql
+\i docker-entrypoint-initdb.d/Item.sql
+\i docker-entrypoint-initdb.d/NPC.sql
+\i docker-entrypoint-initdb.d/NPCUsaItem.sql
+\i docker-entrypoint-initdb.d/NPCEquipaArma.sql
