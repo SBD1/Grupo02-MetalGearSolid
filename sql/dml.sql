5ea562d675cd2ef0d9cdce5588ffdd4eb8cd60d5
Joao Henrique <joao.henrique1299@hotmail.com>
2022-09-05 09:19:20 -0300

    Atualizando projeto, resolvendo conflitos com triggers

--- sql/dml.sql
+++ sql/dml.sql
@@ -1,16 +1,12 @@
-\i docker-entrypoint-initdb.d/inserts/Player.sql
+\i docker-entrypoint-initdb.d/inserts/Uniforme.sql
 \i docker-entrypoint-initdb.d/inserts/Arma.sql
-\i docker-entrypoint-initdb.d/inserts/Recurso.sql
-\i docker-entrypoint-initdb.d/inserts/Unidade.sql
+\i docker-entrypoint-initdb.d/inserts/Player.sql
 \i docker-entrypoint-initdb.d/inserts/codinome.sql
 \i docker-entrypoint-initdb.d/inserts/Terreno.sql
 \i docker-entrypoint-initdb.d/inserts/Missao.sql
 \i docker-entrypoint-initdb.d/inserts/Mapa.sql
-\i docker-entrypoint-initdb.d/inserts/Uniforme.sql
 \i docker-entrypoint-initdb.d/inserts/UniformeCamuflaTerreno.sql
---\i docker-entrypoint-initdb.d/inserts/Item.sql --CRIAR UM TRIGGER PARA A TABELA DE ITEM
 \i docker-entrypoint-initdb.d/inserts/NPC.sql
-\i docker-entrypoint-initdb.d/inserts/NPCUsaItem.sql
 \i docker-entrypoint-initdb.d/inserts/NPCEquipaArma.sql
 \i docker-entrypoint-initdb.d/inserts/MapaTemMapa.sql
 \i docker-entrypoint-initdb.d/inserts/MapaPosicionaRecurso.sql
--- sql/inserts/Unidade.sql
+++
@@ -1,8 +0,0 @@
-insert into Unidade
-(idPlayer,nivel,maxSoldados,qtdSoldados,tipo)
-values
-(1,0,100,0,'COMBATE'),
-(1,0,100,0,'MEDICA'),
-(1,0,100,0,'INTELIGENCIA'),
-(1,0,100,0,'DESENVBASE'),
-(1,0,100,0,'SUPORTE');
